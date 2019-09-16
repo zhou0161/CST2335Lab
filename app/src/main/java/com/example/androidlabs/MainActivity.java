e8d9dea422c1676ca8a4a1ee9ecc57a1771c5edd
CST2335 <zhou0161@algonquinlive.com>
2019-09-16 19:19:01 -0400

    initial

---
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
+//        setContentView(R.layout.activity_main_relative);
+    }
+}
