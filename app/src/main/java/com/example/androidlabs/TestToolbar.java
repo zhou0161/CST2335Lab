57b74243a3a7c608267fc590301cee3393874f8b
CST2335 <zhou0161@algonquinlive.com>
2019-11-12 11:31:30 -0500

    lab 7

--- app/src/main/java/com/example/androidlabs/TestToolbar.java
+++ app/src/main/java/com/example/androidlabs/TestToolbar.java
@@ -1,14 +1,25 @@
 package com.example.androidlabs;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 
 public class TestToolbar extends AppCompatActivity {
+
+    String message = "This is the initial message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,4 +36,52 @@ public class TestToolbar extends AppCompatActivity {
         inflater.inflate(R.menu.toolbar_menu, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Button btn = findViewById(R.id.toolbarBtn);
+        switch(item.getItemId())
+        {
+            //what to do when the menu item is selected:
+            case R.id.item1:
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item2:
+                alertExample();
+                break;
+            case R.id.item3:
+                Snackbar.make(btn, "Go Back?", Snackbar.LENGTH_LONG).setAction("Go Back", c ->{
+                    finish();
+                }).show();
+                break;
+            case R.id.item4:
+                Toast.makeText(this, "You clicked on the overflow menu", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
+    }
+
+    public void alertExample()
+    {
+        View middle = getLayoutInflater().inflate(R.layout.view_extra_stuff, null);
+//        Button btn = (Button)middle.findViewById(R.id.view_button);
+        EditText et = (EditText)middle.findViewById(R.id.view_edit_text);
+//        btn.setOnClickListener( clk -> et.setText("You clicked my button!"));
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("The Message")
+                .setPositiveButton("Positive", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        message = et.getText().toString();
+                    }
+                })
+                .setNegativeButton("Negative", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // What to do on Cancel
+                    }
+                }).setView(middle);
+
+        builder.create().show();
+    }
+
 }
