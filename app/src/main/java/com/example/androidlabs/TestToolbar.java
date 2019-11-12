7532b870c98f818808eab0aaa3cabf4a8d0173a1
CST2335 <zhou0161@algonquinlive.com>
2019-11-12 02:16:58 -0500

    lab 7

--- app/src/main/java/com/example/androidlabs/TestToolbar.java
+++ app/src/main/java/com/example/androidlabs/TestToolbar.java
@@ -1,8 +1,12 @@
 package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 
 public class TestToolbar extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class TestToolbar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_toolbar);
+
+        Toolbar tBar = findViewById(R.id.toolbar);
+        setSupportActionBar(tBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
